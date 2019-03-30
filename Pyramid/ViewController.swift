a263d466bad015f6846f6a76a08a7141d7a78958
Hacker719 <neel.redkar@gmail.com>
2019-03-30 00:24:35 -0700

    Add Icons

--- Pyramid/ViewController.swift
+++ Pyramid/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var canvas: CanvasView!
+    @IBAction func clear2(_ sender: Any) {
+        canvas.clearCanvas()
+    }
+    
+    @IBAction func clear3(_ sender: Any) {
+        canvas.clearCanvas()
+    }
+    @IBAction func clear(_ sender: Any) {
+        canvas.clearCanvas()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
